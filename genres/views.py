f8293867fa2a9f2629246976ee3860229751073e
joaocarlosms <jcmeseverino@gmail.com>
2024-05-06 10:35:46 -0300

    feat: add genre_detail_view with methods update(PUT) and delete

--- genres/views.py
+++ genres/views.py
@@ -10,6 +10,7 @@ def genres_create_list_view(request):
         genres = Genre.objects.all()
         data = [{'id': genre.id, 'name': genre.name} for genre in genres]
         return JsonResponse(data, safe=False)
+        
     elif request.method == 'POST':
         data = json.loads(request.body.decode('utf-8'))
         genres = Genre(name=data['name'])
@@ -22,5 +23,21 @@ def genres_create_list_view(request):
 @csrf_exempt
 def genre_detail_view(request, pk):
     genres = get_object_or_404(Genre, pk=pk)
-    data = {'id': genres.id, 'name': genres.name}
-    return JsonResponse(data)
+
+    if request.method == 'GET':
+        data = {'id': genres.id, 'name': genres.name}
+        return JsonResponse(data)
+
+    elif request.method == 'PUT':
+        data = json.loads(request.body.decode('utf-8'))
+        genres.name = data['name']
+        genres.save()
+        return JsonResponse(
+            {'id': genres.id, 'name': genres.name}
+        )
+    elif request.method == 'DELETE':
+        genres.delete()
+        return JsonResponse(
+            {'message': 'Gênero excluído com sucesso!'},
+            status=204
+        )
