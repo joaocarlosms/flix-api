221bcb9972ee65d1668b717bee2f1cf65272c51b
joaocarlosms <jcmeseverino@gmail.com>
2024-05-06 09:38:42 -0300

    feat: add new genre_detail and modify genre_list on view

--- genres/urls.py
+++ genres/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from genres.views import genres_view
+from genres.views import genre_detail_view, genres_create_list_view
 
 urlpatterns = [
-    path('genres/', genres_view, name='genres'),
+    path('genres/', genres_create_list_view, name='genres'),
+    path('genres/<int:pk>/', genre_detail_view, name='genres_detail'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -2,9 +2,10 @@ from django.http import JsonResponse
 from genres.models import Genre
 import json
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 
 @csrf_exempt
-def genres_view(request):
+def genres_create_list_view(request):
     if request.method == 'GET':
         genres = Genre.objects.all()
         data = [{'id': genre.id, 'name': genre.name} for genre in genres]
@@ -17,3 +18,9 @@ def genres_view(request):
             {'id': genres.id, 'name': genres.name}, 
             status=201
         )
+
+@csrf_exempt
+def genre_detail_view(request, pk):
+    genres = get_object_or_404(Genre, pk=pk)
+    data = {'id': genres.id, 'name': genres.name}
+    return JsonResponse(data)
