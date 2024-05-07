90d4a082dcda3badcd03d9f11a86687b2a62e05d
joaocarlosms <jcmeseverino@gmail.com>
2024-05-07 10:48:14 -0300

    feat: add new model Actor

---
+++ actors/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+NATIONALITY_CHOICES = (
+    ('BRAZIL', 'Brasil'),
+    ('USA', 'Estados Unidos'),
+    ('UK', 'Reino Unido'),
+    ('JAPAN', 'Japão'),
+    ('ARG', 'Argentina')
+)
+
+class Actor(models.Model):
+    name = models.CharField(max_length=200)
+    birthday = models.DateField(blank=True, null=True)
+    nationality = models.CharField(
+        max_length=100,
+        choices=NATIONALITY_CHOICES,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
