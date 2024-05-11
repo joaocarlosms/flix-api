78b7e26c14a2f8ad769bd30df0c2db2d0bb733b8
joaocarlosms <jcmeseverino@gmail.com>
2024-05-10 21:36:05 -0300

    feat: add avarage for movies

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,8 +1,30 @@
 from rest_framework import serializers
 from movies.models import Movie
+from django.db.models import Avg
 
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+
+        return None
+
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError('A data de lançamento não pode ser inferior a 1990!')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError('Resumo não deve ser maior que 200 caracteres')
+        return value
+    
+    
