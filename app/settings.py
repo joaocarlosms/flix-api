26303b6a16338022d979bb598ca61269b65095ec
joaocarlosms <jcmeseverino@gmail.com>
2024-05-11 17:33:45 -0300

    feat: add authentication

--- app/settings.py
+++ app/settings.py
@@ -37,11 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    'rest_framework_simplejwt',
+    
     'genres',
     'actors',
     'movies',
     'reviews',
-    'rest_framework',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
--- app/urls.py
+++ app/urls.py
@@ -4,11 +4,14 @@ import genres
 import actors
 import movies
 import reviews
+import authentication
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     path('api/v1/', include('genres.urls')),
     path('api/v1/', include('actors.urls')),
     path('api/v1/', include('movies.urls')),
     path('api/v1/', include('reviews.urls')),
+    path('api/v1/', include('authentication.urls')),
 ]
