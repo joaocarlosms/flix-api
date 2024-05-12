d02540d385e13b47d3fda7bcf4b97a30348a3712
joaocarlosms <jcmeseverino@gmail.com>
2024-05-12 20:19:31 -0300

    add permissions on urls

--- genres/views.py
+++ genres/views.py
@@ -1,11 +1,14 @@
 from genres.models import Genre
 from rest_framework import generics
 from genres.serializers import GenreSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class GenreCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 class GenreRetriveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
 from movies.models import Movie
 from movies.serializers import MovieSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class MovieListCreateView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
 class MovieRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer    
